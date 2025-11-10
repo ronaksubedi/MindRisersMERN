5a4a5662c41d2f9a78b91e25571c55c112868dac
Ronak Subedi <subedironak@gmail.com>
2025-11-10 19:49:02 +0545

    Dom pratice

--- JavaScript/DOM/script.js
+++ JavaScript/DOM/script.js
@@ -2,10 +2,18 @@
 let btn = document.querySelector("button");
 btn.addEventListener("click", () => {
     if(document.querySelector("body").style.backgroundColor === "black"){
-        document.querySelector("body").style.backgroundColor = "white";
-        document.querySelector("body").style.color = "black";
+        document.querySelector("body").style.backgroundColor = "white" ;
+        document.querySelector("body").style.color = "black" ;
+        document.querySelector("#intro").style.color = "none";
         return;
     }
     document.querySelector("body").style.backgroundColor = "black";
+    document.querySelector("h1").style.color = "white";
+    document.querySelector("h2").style.color = "white";
+    document.querySelector("p").style.color = "white";
+    document.querySelector("button").style.color = "blue";
+    document.querySelector("button").style.backgroundColor = "black";
     document.querySelector("body").style.color = "white";
+    document.querySelector("button").style.backgroundColor = "white";
+    document.querySelector("#intro").style.color = "none";
 });
