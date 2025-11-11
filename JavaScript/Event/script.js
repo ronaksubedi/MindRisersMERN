87b29146c12b3ad8a6846745124b7d6a6cd260ac
Ronak Subedi <subedironak@gmail.com>
2025-11-11 16:51:44 +0545

    event

---
+++ JavaScript/Event/script.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById("btn");
+btn.addEventListener("click", function() {
+    alert("Button Clicked!");
+});
+const secBtn = document.getElementById("btn2");
+secBtn.addEventListener("dblclick", function() {
+    alert("Second Button Clicked!");
+});
