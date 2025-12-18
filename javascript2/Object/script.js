9dff54470564ed5ab708ac915df75886b85531cc
Ronak Subedi <subedironak@gmail.com>
2025-12-18 11:49:46 +0545

    push all the remaining changes

--- javascript2/Object/script.js
+++ javascript2/Object/script.js
@@ -1,2 +1,60 @@
-const random = Math.floor(Math.random() * 31) + 20;
-console.log(random);
+// const random = Math.floor(Math.random() * 31) + 20;
+// console.log(random);
+// car product computer house
+// const car = {
+//   carName: "BMW",
+//   model: "2022",
+//   manufacturer: "Toyota Motors",
+//   carinfo: function () {
+//     console.log(
+//       `The ${this.carName} is maufactured by ${this.manufacturer} in year ${this.model}`
+//     );
+//   },
+// };
+// car.carinfo();
+// const products = {
+//   productName: "Louis Vuitton",
+//   type: "luxury",
+// };
+// const Product = {
+//   pName: "Accer Laptop",
+//   model: "2022",
+//   processor: "Inter",
+//   pinfo: function () {
+//     console.log(
+//       `The ${this.pName} used ${this.processor} in year ${this.model}`
+//     );
+// //   },
+// // };
+// // Product.pinfo();
+// // const Laptop = {
+// //   productName: "Louis Vuitton",
+// //   type: "luxury",
+// // };
+
+// const fields = ["page", "search", "sort"];
+
+// const obj = {
+//   page: 1,
+//   search: "hello",
+//   sort: "ascending",
+//   rating: 5,
+//   title: "great",
+// };
+
+// console.log("Original object:", obj);
+// fields.forEach((field) => {
+//   delete obj[field];
+// });
+
+// console.log("Object after deleting page, search, and sort:", obj);
+
+// function car(name, model, company) {
+//   this.name = name;
+//   this.model = model;
+//   this.company = company;
+// }
+
+// const car1 = new car("Toyta", 2025, "Toytamodel");
+
+// console.log(car1);
